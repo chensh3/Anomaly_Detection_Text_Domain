0e089d33cc5235c619e6717a274acfb6c26729ff
chensheiner-SX <chen@sightx.ai>
2023-05-01 09:48:31 +0300

    add data visualisation for results

--- results_analysis.py
+++ results_analysis.py
@@ -1,25 +1,25 @@
 import numpy as np
 import pandas as pd
 
-df=pd.read_excel("results_initial_run.xlsx",skiprows=1,engine="openpyxl")
+df = pd.read_excel("results_initial_run.xlsx", skiprows=1, engine="openpyxl")
 
-df=df.rename(columns={"Unnamed: 0":"dataset_name","COPOD":"COPOD_ROC","DeepSVDD":"DeepSVDD_ROC","COPOD.1":"COPOD_PR","DeepSVDD.1":"DeepSVDD_PR"})
+df = df.rename(
+    columns={"Unnamed: 0": "dataset_name", "COPOD": "COPOD_ROC", "DeepSVDD": "DeepSVDD_ROC", "COPOD.1": "COPOD_PR",
+             "DeepSVDD.1": "DeepSVDD_PR"})
 
-df.set_index("dataset_name",inplace=True)
-
+df.set_index("dataset_name", inplace=True)
 
 for ind in df.index:
-    name=ind.split("_")
+    name = ind.split("_")
 
-    diff=name[0]
-    hamm=name[1]
-    vectors=[name[2],name[3]]
-    classes=[]
-    for cls in name[4:]:
-        classes.append(cls)
+    diff = name[0]
+    hamm = name[1]
+    vectors = [name[2], name[3]]
+    classes = name[4:]
 
-    df.loc[ind,["difficulty","hamming_value","vectors","classes"]]=[diff,hamm,vectors,classes]
-
+    df.loc[ind, ["difficulty", "hamming_value", "vectors", "classes"]] = [diff, hamm, vectors, classes]
 
 for hamm in df.hamming_value.unique():
-    print(f"\n Avg Results:\n{df[df.hamming_value==hamm].mean()}\n")
+    print(
+        f'\n Avg Results - hamming value of: {hamm}\n{df[df.hamming_value == hamm].loc[:, ["COPOD_ROC", "DeepSVDD_ROC", "COPOD_PR", "DeepSVDD_PR"]].apply(lambda x: [x.mean(), x.std()])}\n')
+    # print(f'\n Avg Results:\n{df[df.hamming_value==hamm].mean()}\n')
