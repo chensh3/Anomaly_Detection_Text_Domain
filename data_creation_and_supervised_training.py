c7a8e2306c38992ccc7f6e401766f33366a330fc
chensh3 <chen.sheiner@gmail.com>
2023-06-01 23:00:43 +0300

    add balance training and whole pipline of testing upper bound datasets values

--- data_creation_and_supervised_training.py
+++ data_creation_and_supervised_training.py
@@ -8,23 +8,23 @@ from pycaret.classification import ClassificationExperiment
 from pycaret.classification import *
 import pycaret
 from time import perf_counter
-
+from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegressionCV
 
 SAMPLE_LOWER_LIMIT = 50
 test_size = 0.3
-
+CV_FOLD = 5
+SEED = 42
 attributes = pd.read_excel("attributes.xlsx")
-attributes.rename(columns={"Unnamed: 0": "classes"}, inplace=True)
+attributes.rename(columns = {"Unnamed: 0": "classes"}, inplace = True)
 num_classes = 34
-attributes.set_index("classes", inplace=True)
+attributes.set_index("classes", inplace = True)
 
-classes = pd.DataFrame(np.zeros((num_classes, num_classes)), index=attributes.index, columns=attributes.index,
-                       dtype=int)
+classes = pd.DataFrame(np.zeros((num_classes, num_classes)), index = attributes.index, columns = attributes.index,
+                       dtype = int)
 for i in range(num_classes):
     classes.iloc[i, i] = 1
-attributes = pd.concat([classes, attributes], axis=1)
-
-
+attributes = pd.concat([classes, attributes], axis = 1)
 
 print("Loading data df...")
 df = pd.read_pickle("short_df_all_192.pickle")
@@ -32,15 +32,59 @@ print("Done loading data")
 
 permutations = list(itertools.combinations_with_replacement(attributes.columns, 2))
 dataset_df = pd.DataFrame(
-    columns=["Dataset_name", "Anomaly_classes", "Training_sample_size",
-             "Test_sample_size",
-             "Test_balance_size",
-             "Test_length_dev",
-             "Anomaly_All_ratio_dev",
-             "Test_size_balance_dev",
-             "Test_All_ratio_dev",
-             "Test_Train_ratio_dev",
-             "Anomaly_test_ratio_dev"])
+    columns = ["Dataset_name", "Anomaly_classes", "Training_sample_size",
+               "Test_sample_size",
+               "Test_balance_size",
+               "Test_length_dev",
+               "Anomaly_All_ratio_dev",
+               "Test_size_balance_dev",
+               "Test_All_ratio_dev",
+               "Test_Train_ratio_dev",
+               "Anomaly_test_ratio_dev"])
+non_balance_results = pd.DataFrame(
+    columns = ["Dataset_name",
+               "acc_avg",
+               "acc_std",
+               "acc_min",
+               "acc_max",
+               "acc_min_name",
+               "acc_max_name",
+               "auc_avg",
+               "auc_std",
+               "auc_min",
+               "auc_max",
+               "auc_min_name",
+               "auc_max_name",
+               "f1_avg",
+               "f1_std",
+               "f1_min",
+               "f1_max",
+               "f1_min_name",
+               "f1_max_name",
+
+               ])
+balance_results = pd.DataFrame(
+    columns = ["Dataset_name",
+               "acc_avg",
+               "acc_std",
+               "acc_min",
+               "acc_max",
+               "acc_min_name",
+               "acc_max_name",
+               "auc_avg",
+               "auc_std",
+               "auc_min",
+               "auc_max",
+               "auc_min_name",
+               "auc_max_name",
+               "f1_avg",
+               "f1_std",
+               "f1_min",
+               "f1_max",
+               "f1_min_name",
+               "f1_max_name",
+
+               ])
 
 
 def save_to_dataframe(dataset_data, db_name, cls_list, a_test, n_test, n_train):
@@ -62,29 +106,110 @@ def save_to_dataframe(dataset_data, db_name, cls_list, a_test, n_test, n_train):
     return dataset_data
 
 
-def train_supervised(name,train, test):
-    print("start train function")
-    train_data=train.drop(columns=["short_description","category"])
-    test_data=test.drop(columns=["short_description","category"])
+def balance_test(test):
+    len_anomalies = int(test.label.sum())
+    len_normal = len(test) - len_anomalies
+    min_ind = np.argmin([len_normal, len_anomalies])
+    if min_ind == 1:
+        normal = test[test.label == 0].sample(len_anomalies)
+        anomaly = test[test.label == 1]
+
+    else:
+        anomaly = test[test.label == 1].sample(len_normal)
+        normal = test[test.label == 0]
+    assert len(normal) == len(anomaly) == [len_normal, len_anomalies][min_ind], \
+        f"Wrong length: {len(normal), len(anomaly), [len_normal, len_anomalies][min_ind]}"
+    balance_test = pd.concat([anomaly, normal])
+    return balance_test
+
+
+def balance_train(data):
+    len_anomalies = int(data.label.sum())
+    len_normal = len(data) - len_anomalies
+    min_ind = np.argmin([len_normal, len_anomalies])
+    if [len_normal, len_anomalies][min_ind] < 1000:
+        return None
+    elif [len_normal, len_anomalies][min_ind] > 10000:
+        a = data[data.label == int(not min_ind)].sample([len_normal, len_anomalies][min_ind])
+        b = data[data.label == min_ind]
+        balance_train = pd.concat([a, b])
+    else:
+        if min_ind == 1:
+            oversample = SMOTE()
+            if len_normal / len_anomalies <= 1.3:
+                balance_train, y = oversample.fit_resample(data.drop(columns = "label"), data.label)
+                balance_train["label"] = y
+            else:
+                normal = data[data.label == 0].sample(int(len_anomalies * 1.3))
+                anomaly = data[data.label == 1]
+                balance_train = pd.concat([anomaly, normal])
+                balance_train, y = oversample.fit_resample(balance_train.drop(columns = "label"), balance_train.label)
+                balance_train["label"] = y
+        else:
+            oversample = SMOTE()
+            if len_anomalies / len_normal <= 1.3:
+                balance_train, y = oversample.fit_resample(data.drop(columns = "label"), data.label)
+                balance_train["label"] = y
+            else:
+                anomaly = data[data.label == 1].sample(int(len_normal * 1.3))
+                normal = data[data.label == 0]
+                balance_train = pd.concat([anomaly, normal])
+                balance_train, y = oversample.fit_resample(balance_train.drop(columns = "label"), balance_train.label)
+                balance_train["label"] = y
+
+    return balance_train
+
+
+def get_info_from_results(results):
+    info = np.array([])
+    for col in ["Accuracy", "AUC", "F1"]:
+        info = np.append(info, [results[col].mean(),
+                                results[col].std(),
+                                results[col].min(),
+                                results[col].max(),
+                                results.index[results[col].argmin()],
+                                results.index[results[col].argmax()]]
+                         )
+
+    return info
+
+
+def model_train(train, test):
     print("start setup")
     start = perf_counter()
-    s = setup(train_data, test_data=test_data, target='label', session_id=42,index=False,fold=5,preprocess=False
-              ,log_experiment = False,
+    s = setup(train, test_data = test, target = 'label', session_id = SEED, index = False,
+              fold = CV_FOLD,
+              preprocess = False,
+              log_experiment = False,
+              use_gpu=True,
               # experiment_name = name,
-              fix_imbalance=True)
-    print(f"setup time: {perf_counter()-start}")
-        # clf1.setup(data=train.loc[:, ["embed","label"]], target='label',
-    #            session_id=42,log_experiment = True, experiment_name = 'dataset')
-    # start = perf_counter()
-    # best_model = compare_models(include = ['lr'])
+              fix_imbalance = False)
+    print(f"setup time: {perf_counter() - start}")
     best_model = compare_models()
     print(f"model train time: {perf_counter() - start}")
     results = pull()
     return results, best_model
 
 
+def train_supervised(train, test):
+    print("start train function")
+    train_data = train.drop(columns = ["short_description", "category"])
+    test_data = test.drop(columns = ["short_description", "category"])
+    balance_train_df = balance_train(train_data)
+
+    non_balance_results, _ = model_train(balance_train_df, test_data)
+    balance_test_df = balance_test(test_data)
+
+    balance_results, _ = model_train(balance_train_df, balance_test_df)
+
+    balance_info = get_info_from_results(balance_results)
+    non_balance_info = get_info_from_results(non_balance_results)
+
+    return non_balance_info, balance_info
+
+
 count = 0
-for i, per in tqdm(enumerate(permutations[:1])):
+for i, per in tqdm(enumerate(permutations[50:51])):
 
     # print("Working on per of: {per}")
     cls = np.array([])
@@ -95,7 +220,7 @@ for i, per in tqdm(enumerate(permutations[:1])):
 
     data = df.copy()
 
-    anomaly = pd.DataFrame(columns=df.columns)
+    anomaly = pd.DataFrame(columns = df.columns)
     for cls_1 in cls:
         anomaly = anomaly.append(data.loc[(data.category == cls_1), :])
 
@@ -103,8 +228,8 @@ for i, per in tqdm(enumerate(permutations[:1])):
     anomaly.loc[:, "label"] = np.ones(len(anomaly))
     normal = data.drop(anomaly.index)
     normal.loc[:, "label"] = np.zeros(len(normal))
-    normal_train, normal_test = train_test_split(normal, test_size=test_size)
-    anomaly_train, anomaly_test = train_test_split(anomaly, test_size=test_size)
+    normal_train, normal_test = train_test_split(normal, test_size = test_size)
+    anomaly_train, anomaly_test = train_test_split(anomaly, test_size = test_size)
 
     if len(anomaly_test) <= SAMPLE_LOWER_LIMIT:
         print(f"Not Enough Samples in Anomaly-Test, {per}")
@@ -123,33 +248,42 @@ for i, per in tqdm(enumerate(permutations[:1])):
         test = pd.concat([anomaly_test, normal_test])
         sup_train = pd.concat([anomaly_train, normal_train])
         print("go to train models")
-        results, best_model = train_supervised(name,sup_train, test)
+        non_balance, balance = train_supervised(sup_train, test)
+        non_balance_results.loc[len(non_balance_results), :] = np.append(name, non_balance)
+        balance_results.loc[len(balance_results), :] = np.append(name, balance)
+        non_balance_results.to_csv("non_balance_results.csv")
+        balance_results.to_csv("balance_results.csv")
 
     # INVERT:
 
-    # # anomaly = normal.copy()
-    # data = df.copy()
-    # normal = temp.copy()
-    # anomaly = data.drop(normal.index)
-    #
-    # normal.loc[:, "label"] = np.zeros(len(normal))
-    # anomaly.loc[:, "label"] = np.ones(len(anomaly))
-    #
-    # normal_train, normal_test = train_test_split(normal, test_size=test_size)
-    # anomaly_train, anomaly_test = train_test_split(anomaly, test_size=test_size)
-    #
-    # if len(anomaly_test) <= SAMPLE_LOWER_LIMIT:
-    #     print(f"Not Enough Samples in Anomaly-Test,{i} {per}")
-    #     continue
-    # elif len(normal_test) <= SAMPLE_LOWER_LIMIT:
-    #     print(f"Not Enough Samples in Normal-Test,{i} {per}")
-    #     continue
-    # else:
-    #     count += 1
-    #     name = f"{count}_{'_'.join(set(per))}_INVERT"
-    #     cls = anomaly.category.unique()
-    #     dataset_df = save_to_dataframe(dataset_df, name, cls, anomaly_test, normal_test, normal_train)
-    #     dataset_df.to_csv("dataset_df.csv")
-    #     test = pd.concat([anomaly_test, normal_test]).sample(frac=1).reset_index(drop=True)
-    #     sup_train = pd.concat([anomaly_train, normal_train]).sample(frac=1).reset_index(drop=True)
-    #     # results, best_model = train_supervised(name,sup_train, test)
+    # anomaly = normal.copy()
+    data = df.copy()
+    normal = temp.copy()
+    anomaly = data.drop(normal.index)
+
+    normal.loc[:, "label"] = np.zeros(len(normal))
+    anomaly.loc[:, "label"] = np.ones(len(anomaly))
+
+    normal_train, normal_test = train_test_split(normal, test_size=test_size)
+    anomaly_train, anomaly_test = train_test_split(anomaly, test_size=test_size)
+
+    if len(anomaly_test) <= SAMPLE_LOWER_LIMIT:
+        print(f"Not Enough Samples in Anomaly-Test,{i} {per}")
+        continue
+    elif len(normal_test) <= SAMPLE_LOWER_LIMIT:
+        print(f"Not Enough Samples in Normal-Test,{i} {per}")
+        continue
+    else:
+        count += 1
+        name = f"{count}_{'_'.join(set(per))}_INVERT"
+        cls = anomaly.category.unique()
+        dataset_df = save_to_dataframe(dataset_df, name, cls, anomaly_test, normal_test, normal_train)
+        dataset_df.to_csv("dataset_df.csv")
+        test = pd.concat([anomaly_test, normal_test])
+        sup_train = pd.concat([anomaly_train, normal_train])
+        print("go to train models")
+        non_balance, balance = train_supervised(sup_train, test)
+        non_balance_results.loc[len(non_balance_results), :] = np.append(name, non_balance)
+        balance_results.loc[len(balance_results), :] = np.append(name, balance)
+        non_balance_results.to_csv("non_balance_results.csv")
+        balance_results.to_csv("balance_results.csv")
